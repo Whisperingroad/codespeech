023bb843c846898fe306cd34fa5b7ca3fcaa7342
Whisperingroad <sebastian.wong@hotmail.com>
2016-02-08 19:12:27 +0800

    updated

--- codespeech/src/corpusgenerator/SentenceTagger.java
+++ codespeech/src/corpusgenerator/SentenceTagger.java
@@ -7,6 +7,8 @@ public class SentenceTagger {
 
 	ArrayList<String> templateSent = new ArrayList<String>();
 	ArrayList<String> formattedSent = new ArrayList<String>();
+	ArrayList<String> declarationDependencies = new ArrayList<String>();
+	String tab = "\t";
 	
 	public ArrayList<String> tag2SubjectIfStatement(ArrayList<String> sents)
 	{
@@ -38,4 +40,52 @@ public class SentenceTagger {
 		}
 		return formattedSent;
 	}
+	
+	public ArrayList<String> tagDeclarationStatements(ArrayList<String> sentences)
+	{
+		// Common Declaration statements
+
+		int sentenceNumber = 0;
+		int sentenceIndex = 0;
+		// four words
+		for (String s : sentences)
+		{
+			sentenceNumber++;
+			sentenceIndex++;
+			if (sentenceIndex > 16)
+				sentenceIndex = 1;
+			
+			String dependency = "";
+			ArrayList<String> words = new ArrayList<String>(Arrays.asList(s.split(" ")));
+			for (int i = 0; i < words.size(); i++)
+			{
+				if (words.size() == 4)
+				{
+					// Create/Declare
+					if (i == 0)
+						dependency = (i+1) + tab + words.get(i) + tab + words.get(i) + tab + "VERB" + tab + "VB" + tab + "VerbForm=Inf" + tab +
+						"0" + tab + "root" + tab + "_" + tab + "_";
+					// a or an
+					else if (i==1)
+						dependency = (i+1) + tab + words.get(i) + tab + words.get(i) + tab + "DET" + tab + "DT" + tab + "Definite=Ind|PronType=Art" + tab
+						+ "4" + tab + "det" + tab + "_" + tab + "_";
+					else if (i == 2)
+						dependency = (i+1) + tab + words.get(i) + tab + words.get(i) + tab + "ADJ" + tab + "JJ" + tab + "_" + tab
+						+ "4" + tab + "amod" + tab + "_" + tab + "_";
+					else if (i == 3)
+						dependency = (i+1) + tab + words.get(i) + tab + words.get(i) + tab + "NOUN" + tab + "NN" + tab + "Number=SING" + tab
+						+ "1" + tab + "dobj" + tab + "_" + tab + "_";
+				}
+				
+			}
+			sentenceNumber++; 
+		}
+		return sentences;
+	}
+	
+	
+	
+	
+	
+	
 }
